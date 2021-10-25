961b852dfb6da5882e5222fa34db636b9c0034b3
anzu takahashi <anzu@happy-girl.jp>
2021-10-25 16:27:17 +0900

    onkkではこういうふうにしたい

--- calc.py
+++ calc.py
@@ -1,10 +1,18 @@
 import datetime
 
 now = datetime.datetime.now()
-last = datetime.datetime(2021, 9, 29, 0, 0, 0)
-start = datetime.datetime(2021, 8, 29, 0, 0, 0)
-pa = now - start
-re = last - now
-print("passed {} ".format(pa))
-print("remain {} ".format(re))
+start = [
+  {"id": 1, "dt": datetime.datetime(2021, 8, 29, 0, 0, 0)},
+  {"id": 2, "dt": datetime.datetime(2021, 10, 24, 0, 0, 0)},
+  {"id": 3, "dt": datetime.datetime(2021, 11, 24, 0, 0, 0)}
+]
+
+for i in start:
+  if i['id'] == 1:
+    old = i
+    continue
+  pa = i['dt'] - old['dt']
+  re = i['dt'] - now
+  print("{} ~ {} / passed {} / remain {} ".format(old['dt'], i['dt'], pa, re))
+  old = i
 
