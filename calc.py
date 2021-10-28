5e8be9bf2a007c08b153d44a9e534e7832a57200
anzu takahashi <anzu@happy-girl.jp>
2021-10-28 18:02:11 +0900

    date edit

--- calc.py
+++ calc.py
@@ -4,7 +4,8 @@ now = datetime.datetime.now()
 start = [
   {"id": 1, "dt": datetime.datetime(2021, 8, 29, 0, 0, 0)},
   {"id": 2, "dt": datetime.datetime(2021, 10, 24, 0, 0, 0)},
-  {"id": 3, "dt": datetime.datetime(2021, 11, 24, 0, 0, 0)}
+  {"id": 3, "dt": datetime.datetime(2021, 10, 28, 11, 0, 0)},
+  {"id": 4, "dt": datetime.datetime(2021, 11, 28, 0, 0, 0)}
 ]
 
 for i in start:
@@ -13,6 +14,8 @@ for i in start:
     continue
   pa = i['dt'] - old['dt']
   re = i['dt'] - now
-  print("{} ~ {} / passed {} / remain {} ".format(old['dt'], i['dt'], pa, re))
+  print("{} ~ {} / passed {}".format(old['dt'], i['dt'], pa))
   old = i
+
+print("remain {} ".format(re))
 
